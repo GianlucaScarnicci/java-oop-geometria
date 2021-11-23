b263548d61f6939a7018be918710a625a5f3af76
GianlucaScarnicci <giangiscarnicci@gmail.com>
2021-11-23 16:06:24 +0100

    aggiunta condizione base e altezza

--- src/org/generation/italy/geometria/Main.java
+++ src/org/generation/italy/geometria/Main.java
@@ -7,10 +7,15 @@ public class Main {
 	public static void main(String[] args) {
 		Rettangolo rettangolo=new Rettangolo(0,0);
 		Scanner scan=new Scanner(System.in);
-		System.out.print("inserisci la base del rettangolo ");
+		do {
+		System.out.print("inserisci la base del rettangolo positiva ");
 		rettangolo.base=scan.nextInt();
-		System.out.print("inserisci l'altezza del rettangolo ");
+		}while(rettangolo.base<0);
+		do {
+		System.out.print("inserisci l'altezza del rettangolo positiva ");
 		rettangolo.altezza=scan.nextInt();
+		}while(rettangolo.altezza<0 || rettangolo.base<0);
+		
 		System.out.println("l'area è :" +rettangolo.calcolaArea());
 		System.out.println("il perimetro è :" +rettangolo.calcolaPerimetro());
 		
